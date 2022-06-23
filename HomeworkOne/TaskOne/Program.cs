063daa165895d0c8706ebbfe5d9f230681d213eb
SlavaMonochrom <107564935+SlavaMonochrom@users.noreply.github.com>
2022-06-24 02:38:28 +0300

    TaskOne_NotFinal

--- HomeworkOne/TaskOne/Program.cs
+++ HomeworkOne/TaskOne/Program.cs
@@ -1,1 +1,19 @@
 ﻿//на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+int num1;
+int num2;
+
+Console.WriteLine("Введите первое число: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.WriteLine("max = " + num1);
+    Console.WriteLine("max = " + num2);
+}
+else if (num1 == num2)
+{
+        Console.WriteLine("числа равны");
+}
