fd58ca1469503af6458fa4764697280205ef84d8
SlavaMonochrom <107564935+SlavaMonochrom@users.noreply.github.com>
2022-06-24 18:09:07 +0300

    TaskTwoOk

--- HomeworkOne/TaskTwo/Program.cs
+++ HomeworkOne/TaskTwo/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//принимает на вход три числа и выдаёт максимальное из этих чисел
+int num1,num2,num3,max;
+
+Console.WriteLine("Введите первое число: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 >= num2)
+{
+    max = num1;
+}
+else
+{
+    max = num2;
+}
+
+if(num3 > max)
+{
+    max = num3;
+}
+Console.WriteLine("Максимум ранво " +  max);
+
