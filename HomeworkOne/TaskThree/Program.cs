f6501d9f2f25a667f1d964fe2b81e6c57c01c80a
SlavaMonochrom <107564935+SlavaMonochrom@users.noreply.github.com>
2022-06-26 01:47:02 +0300

    TaskThree_Ok

--- HomeworkOne/TaskThree/Program.cs
+++ HomeworkOne/TaskThree/Program.cs
@@ -1,2 +1,17 @@
 ﻿//на вход число и выдаёт, является ли число чётным ?
+int num;
+
+Console.WriteLine ("Введите число: " );
+num = Convert.ToInt32(Console.ReadLine());
+
+ if(num%2 ==0)
+ {
+    Console.WriteLine ("Число четное");
+ }
+ else
+    Console.WriteLine("Число не четное");
+
+
+
+
 
