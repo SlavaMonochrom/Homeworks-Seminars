841ec9b98117b45cd2902d1c298a93e5ee04ec63
SlavaMonochrom <107564935+SlavaMonochrom@users.noreply.github.com>
2022-06-26 03:46:40 +0300

    TaskFourNotFinal

--- HomeworkOne/TaskFour/Program.cs
+++ HomeworkOne/TaskFour/Program.cs
@@ -1,1 +1,18 @@
 ﻿// на вход принимает число (N), на выходе чётные числа от 1 до N.
+
+int n; //even;
+int current;
+current = 0;
+
+
+Console.WriteLine("Введите значение N:  ");
+n = Convert.ToInt32(Console.ReadLine());
+
+
+while(current>=1)
+{
+
+    Console.Write(current + " ");
+    current++;
+}
+//(num%2 ==0)
