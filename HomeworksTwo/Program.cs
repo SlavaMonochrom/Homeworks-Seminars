1d4623345c9d25245bff63e5d3cc98f20159b0f0
SlavaMonochrom <107564935+SlavaMonochrom@users.noreply.github.com>
2022-06-30 20:02:14 +0300

    HomeworkTwo

--- HomeworksTwo/Program.cs
+++ HomeworksTwo/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿//принимает вход трёхзначное на выход вторaя цифра числа
+
+int cutNumber(num)
+{   
+    int num = new Random().Next(100,1000);
+    int ostat = num % 10;
+    int div = num / 10;
+    int result = num;
+    return result;
+}
+
