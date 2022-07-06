f8639ee9d0c6fcea1e014646b7c6c11914a76b14
SlavaMonochrom <107564935+SlavaMonochrom@users.noreply.github.com>
2022-07-07 02:29:59 +0300

    HW4

--- HomeworkFour/Program.cs
+++ HomeworkFour/Program.cs
@@ -29,11 +29,17 @@ Console.WriteLine("Input elements digit : ");
 int digit = Convert.ToInt32(Console.ReadLine());
 int[] intArrayVarriable = new int[digit];
 
-void PrintArray()
-{
-    
 
+void PrintArray(int[]array)   //??? это внос в индекс или в весь массив? 
+{   
+    int count = 0;
+    int m = array.Length;
+    while(m<=count)
+    {
+        int  = new Random().Next(1,10); // как обозначить нулевой индекс массива без указания оного ? 
+        count++;
+    }
+    Console.WriteLine(array); // как правильно вывести в консоль? 
 }
+PrintArray(int []digit); // Как правильно обратиться к методу? 
 
-
-//int value = new Random().Next(1, 11);
